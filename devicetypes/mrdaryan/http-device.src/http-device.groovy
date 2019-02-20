8a2ca2403bf9b433ade7b326491648f8bbfdd82c
David Ryan <daryan@rsp.net.au>
2019-02-20 21:09:17 +1100

    Updated to support on, off and push.

--- devicetypes/mrdaryan/http-device.src/http-device.groovy
+++ devicetypes/mrdaryan/http-device.src/http-device.groovy
@@ -6,22 +6,25 @@
 import groovy.json.JsonSlurper
 
 metadata {
-	definition (name: "HTTP Device", namespace: "mrdaryan", author: "SC") {
+	definition (name: "HTTP Device", namespace: "mrdaryan", author: "David Ryan") {
 	capability "Actuator"
     capability "Switch"
 	capability "Momentary"
-	capability "Sensor"
-     
+	     
     command "PushWithHTTP", ["string"]
-  	}
+    }
 
 	preferences {
 		input("DeviceIP", "string", title:"Device IP Address", description: "Please enter your device's IP Address", required: true, displayDuringSetup: true)
 		input("DevicePort", "string", title:"Device Port", description: "Empty assumes port 80.", required: false, displayDuringSetup: true)
-		input("DevicePath", "string", title:"URL Path", description: "Rest of the URL, include forward slash.", defaultValue: "/jsonrpc", displayDuringSetup: true)
-		input("DeviceBody", "string", title:"Body", description: "Body of message, escape talking marks.", displayDuringSetup: true)
-        input("DeviceContent", "string", title:"Content Type", description: "HTTP Content type.", defaultValue: "application/json", displayDuringSetup: true)
+        input("DeviceContent", "string", title:"Content Type", description: "HTTP Content type", defaultValue: "application/json", displayDuringSetup: true)
 		input(name: "DevicePostGet", type: "enum", title: "POST or GET", options: ["POST","GET"], defaultValue: "POST", required: false, displayDuringSetup: true)
+		input("DevicePathOn", "string", title:"URL Path ON", description: "URL path for ON, include forward slash (i.e. /jsonrpc)", displayDuringSetup: true)
+        input("DeviceBodyOn", "string", title:"Body ON", description: "Body of message.", displayDuringSetup: true)
+        input("DevicePathOff", "string", title:"URL Path OFF", description: "URL path for OFF, include forward slash (i.e. /jsonrpc)", displayDuringSetup: true)
+        input("DeviceBodyOff", "string", title:"Body OFF", description: "Body of message", displayDuringSetup: true)
+        input("DevicePathPush", "string", title:"URL Path PUSH", description: "URL path for PUSH, include forward slash (i.e. /jsonrpc)", displayDuringSetup: true)
+        input("DeviceBodyPush", "string", title:"Body PUSH", description: "Body of message", displayDuringSetup: true)
 		section() {
 			input("HTTPAuth", "bool", title:"Requires User Auth?", description: "Choose if the HTTP requires basic authentication", defaultValue: false, required: true, displayDuringSetup: true)
 			input("HTTPUser", "string", title:"HTTP User", description: "Enter your basic username", required: false, displayDuringSetup: true)
@@ -35,12 +38,16 @@ metadata {
 
 	// UI tile definitions
 	tiles {
-		standardTile("switch", "device.switch", width: 2, height: 2, canChangeIcon: true) {
-			state "off", label: 'Push', action: "momentary.push", backgroundColor: "#ffffff", nextState: "on"
-			state "on", label: 'Push', action: "momentary.push", backgroundColor: "#53a7c0"
+       standardTile("switch", "device.switch", width: 2, height: 2, canChangeIcon: true) {
+    		state "off", label: '${name}', action: "switch.on", icon: "st.switches.switch.off", backgroundColor: "#ffffff", nextState:"on"
+    		state "on", label: '${name}', action: "switch.off", icon: "st.switches.switch.on", backgroundColor: "#00a0dc", nextState:"off"
+        }
+
+		standardTile("button", "device.button", width: 2, height: 2, canChangeIcon: true) {
+            state "default", label: 'Push', action: "push", backgroundColor: "#ffffff"
 		}
 		main "switch"
-		details "switch"
+		details(["switch","button"])
 	}
 }
 
@@ -49,29 +56,24 @@ def parse(String description) {
 }
 
 def on() {
-	push()
+    sendEvent(name: "switch", value: "on", isStateChange: true, display: false)
+    runCmd(DevicePathOn,DeviceBodyOn)
 }
 
 def off() {
-	push()
+    sendEvent(name: "switch", value: "off", isStateChange: true, display: false)
+    runCmd(DevicePathOff,DeviceBodyOff)
 }
 
 def push() {
-    sendEvent(name: "switch", value: "on", isStateChange: true, display: false)
-    sendEvent(name: "switch", value: "off", isStateChange: true, display: false)
-    sendEvent(name: "momentary", value: "pushed", isStateChange: true)
-	runCmd(DevicePath,DeviceBody)
+	runCmd(DevicePathPush,DeviceBodyPush)
 }
 
 def PushWithHTTP(HTTPContent) {
-    sendEvent(name: "switch", value: "on", isStateChange: true, display: false)
-    sendEvent(name: "switch", value: "off", isStateChange: true, display: false)
-    sendEvent(name: "momentary", value: "pushed", isStateChange: true)
-        
     if (DevicePostGet.toUpperCase() == "GET") {
     	runCmd(HTTPContent,"")
 		} else {
-        runCmd(DevicePath,HTTPContent)
+        runCmd(DevicePathPush,HTTPContent)
     }
 }
 
@@ -104,33 +106,26 @@ def runCmd(varCommand,varBody) {
 		log.debug e
 		log.debug "You must not have set the preference for the DevicePOSTGET option"
 	}
-
-	//log.debug "Uses which method: $DevicePostGet"
-	//log.debug "body is: $body"
     log.debug "The device id configured is: $device.deviceNetworkId"
-	log.debug "path is: $path"
-    log.debug "body is: $body"
+	log.debug "Path is: $path"
+    log.debug "Body is: $body"
 	log.debug "The method is $method"
-	try {
-		def hubAction = new physicalgraph.device.HubAction(
-			method: method,
-			path: path,
-			body: body,
-			headers: headers
-			)
-		log.debug hubAction
-		return hubAction
-	}
-	catch (Exception e) {
-		log.debug "Hit Exception $e on $hubAction"
-	}
-    
-    //sendEvent
-    if (varCommand == "off"){
-    	sendEvent(name: "switch", value: "off")
-        log.debug "Executing OFF"
+    //if the body and the path contains nothing, don't worry about sending. 
+    if (body != null && path != null){
+		try {
+			def hubAction = new physicalgraph.device.HubAction(
+				method: method,
+				path: path,
+				body: body,
+				headers: headers
+				)
+			log.debug hubAction
+			return hubAction
+		}
+		catch (Exception e) {
+			log.debug "Hit Exception $e on $hubAction"
+		}
     } else {
-    	sendEvent(name: "switch", value: "on")
-        log.debug "Executing ON"
-    }
+		log.debug "Nothing to send"
+	}
 }
